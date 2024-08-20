2660725b4b9dfc200014788291c3984b33504c95
sohan004 <aryansohan02@gmail.com>
2024-08-21 00:34:35 +0600

    Changes committed on 2024-08-21 00:34:35

--- src/lib/getAllBlogs.js
+++ src/lib/getAllBlogs.js
@@ -1,7 +1,8 @@
 
 const getAllBlogs = async () => {
     const bike = await fetch('https://api.motoviewhub.com/api/filter/search/bike')
-    return await bike.json()
+    const data = await bike.json()
+    return data
 };
 
 export default getAllBlogs;
