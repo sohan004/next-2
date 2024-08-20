947e189c6bcbdf97f2aad4d94941782c6315c3ad
sohan004 <aryansohan02@gmail.com>
2024-08-21 00:58:16 +0600

    Changes committed on 2024-08-21 00:58:16

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -12,12 +12,10 @@ const Page = async () => {
         <div className='grid grid-cols-3 gap-3'>
             <h1>this is blog page</h1>
             {blogs.map((blog) => (
-                <Link href={`/blog/${blog.id}`} key={blog.id}>
-                    <div className='p-2 border hover:bg-slate-400'>
+                    <div key={blog.id} className='p-2 border hover:bg-slate-400'>
                         <h2>{blog.name}</h2>
                         <p>{blog.content}</p>
                     </div>
-                </Link>
             ))}
         </div>
     );
