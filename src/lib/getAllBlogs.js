b604e004aad53e900aead4ac622b19605a398fad
sohan004 <aryansohan02@gmail.com>
2024-08-21 00:29:53 +0600

    Changes committed on 2024-08-21 00:29:53

--- src/lib/getAllBlogs.js
+++ src/lib/getAllBlogs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const getAllBlogs = async () => {
-    const bike = await fetch('https://api.motoviewhub.com/api/filter/search/bike')
+    const bike = await fetch('https://api.motoviewhub.com/api/filter/search/bike', {cache:"no-store"})
     return await bike.json()
 };
 
